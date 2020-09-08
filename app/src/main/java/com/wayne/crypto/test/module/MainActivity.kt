52103f5f3d14a0478e6b8628c36fd7c61902fc9f
wayne.liu <58474926@qq.com>
2020-09-08 23:59:20 +0800

    首页基本UI搭建

--- app/src/main/java/com/wayne/crypto/test/module/MainActivity.kt
+++ app/src/main/java/com/wayne/crypto/test/module/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.wayne.crypto.test.module
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.wayne.crypto.test.R
 import com.wayne.crypto.test.utils.SystemUiUtil
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +14,18 @@ class MainActivity : AppCompatActivity() {
         //暂时默认深色状态栏
         SystemUiUtil.setLightStatusBar(this, false)
         setContentView(R.layout.activity_main)
+        initEvent()
+    }
+
+    private fun initEvent() {
+        bnv_tab.setOnNavigationItemSelectedListener {
+            return@setOnNavigationItemSelectedListener when (it.itemId) {
+                R.id.wallet -> true
+                else -> {
+                    Toast.makeText(this, "developing", Toast.LENGTH_SHORT).show()
+                    false
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/wayne/crypto/test/module/wallet/view/WalletTabFragment.kt
@@ -0,0 +1,9 @@
+package com.wayne.crypto.test.module.wallet.view
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by wayne.liu on 2020/9/8
+ */
+class WalletTabFragment : Fragment() {
+}
