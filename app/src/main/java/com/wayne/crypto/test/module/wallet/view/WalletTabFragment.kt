4804b364b6165720122ff401e64dd31771dcb620
wayne.liu <58474926@qq.com>
2020-09-09 20:18:06 +0800

    添加wallet fragment基本UI实现

--- app/src/main/java/com/wayne/crypto/test/module/wallet/view/WalletTabFragment.kt
+++ app/src/main/java/com/wayne/crypto/test/module/wallet/view/WalletTabFragment.kt
@@ -1,9 +1,18 @@
 package com.wayne.crypto.test.module.wallet.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.wayne.crypto.test.R
 
 /**
  * Created by wayne.liu on 2020/9/8
  */
 class WalletTabFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_wallet, container, false)
+    }
 }
